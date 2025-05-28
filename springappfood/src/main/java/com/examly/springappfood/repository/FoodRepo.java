8fd2e17d6210a8e62a94782a5023e1ef311b5946
Shankar M <68904574+Shankar1307@users.noreply.github.com>
2025-05-28 20:22:27 +0530

    Updated FoodRepo

--- springappfood/src/main/java/com/examly/springappfood/repository/FoodRepo.java
+++ springappfood/src/main/java/com/examly/springappfood/repository/FoodRepo.java
@@ -1,3 +1,10 @@
-public interface FoodRepo {
+package com.examly.springappfood.repository;
+
+import com.examly.springappfood.model.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepo extends JpaRepository<Food, Long> {
     
 }
