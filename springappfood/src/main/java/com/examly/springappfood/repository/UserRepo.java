d39a542cd24df60aaa65776af1cef0beeab8c18c
Shankar M <68904574+Shankar1307@users.noreply.github.com>
2025-05-28 20:21:16 +0530

    Updated UserRepo

--- springappfood/src/main/java/com/examly/springappfood/repository/UserRepo.java
+++ springappfood/src/main/java/com/examly/springappfood/repository/UserRepo.java
@@ -1,3 +1,10 @@
-public class UserRepo {
-    
+package com.examly.springappfood.repository;
+
+import com.examly.springappfood.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
