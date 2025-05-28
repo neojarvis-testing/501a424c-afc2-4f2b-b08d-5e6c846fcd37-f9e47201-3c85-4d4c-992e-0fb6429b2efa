62f31e439750568d8eca84e1de350365ab5ab8eb
Shankar M <68904574+Shankar1307@users.noreply.github.com>
2025-05-28 20:05:55 +0530

    Updated FoodController

--- springappfood/src/main/java/com/examly/springappfood/controller/FoodController.java
+++ springappfood/src/main/java/com/examly/springappfood/controller/FoodController.java
@@ -1,3 +1,42 @@
+package com.examly.springappfood.controller;
+
+import com.examly.springappfood.model.Food;
+import com.examly.springappfood.service.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/food")
 public class FoodController {
-    
+
+    @Autowired
+    private FoodService foodService;
+
+    @PostMapping
+    public ResponseEntity<Food> addFood(@RequestBody Food food) {
+        Food createdFood = foodService.addFood(food);
+        return new ResponseEntity<>(createdFood, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Food> updateFood(@PathVariable Long id, @RequestBody Food food) {
+        Food updatedFood = foodService.updateFood(id, food);
+        return new ResponseEntity<>(updatedFood, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Food> getFoodById(@PathVariable Long id) {
+        Food food = foodService.getFoodById(id);
+        return new ResponseEntity<>(food, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Food>> getAllFoods() {
+        List<Food> foods = foodService.getAllFoods();
+        return new ResponseEntity<>(foods, HttpStatus.OK);
+    }
 }
