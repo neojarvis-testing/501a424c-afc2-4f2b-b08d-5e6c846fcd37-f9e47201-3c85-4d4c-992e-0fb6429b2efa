5b52d2226aa7afa28367b6b1a42f4c2dd3cd198d
Shankar M <68904574+Shankar1307@users.noreply.github.com>
2025-05-28 20:00:56 +0530

    Updated FoodService

--- springappfood/src/main/java/com/examly/springappfood/service/FoodService.java
+++ springappfood/src/main/java/com/examly/springappfood/service/FoodService.java
@@ -1,3 +1,11 @@
-public class FoodService {
-    
+package com.examly.springappfood.service;
+
+import com.examly.springappfood.model.Food;
+import java.util.List;
+
+public interface FoodService {
+    Food addFood(Food food);
+    Food updateFood(Long id, Food food);
+    Food getFoodById(Long id);
+    List<Food> getAllFoods();
 }
